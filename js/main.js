722e9295dd9160a6d139665805a8999e531d423e
Kiva Vlad <vladkiva9@gmail.com>
2020-11-07 22:05:11 +0300

    new update

--- js/main.js
+++ js/main.js
@@ -10,8 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
           categoryItem = document.querySelectorAll(".category__list-item"),
           categoryPages = document.querySelectorAll(".categories__page"),
           productsPages = document.querySelectorAll(".product__page"),
-          categoriesRowItems = document.querySelectorAll(".categories__row-item");
-    let activePage;
+          categoriesRowItems = document.querySelectorAll(".categories__row-item"),
+          productsItem = document.querySelectorAll(".product__item"),
+          modalWindow = document.querySelector(".modal__window"),
+          modalOpacity = document.querySelector(".modal__opacity"),
+          modalWindowImg = document.querySelector(".modal__window-img"),
+          modalWindowTitle = document.querySelector(".modal__window-title"),
+          modalWindowNewprice = document.querySelector(".window__newprice-price"),
+          modalWindowdDuration = document.querySelector(".window__newprice-duration"),
+          modalWindowOldprice = document.querySelector(".window__price-oldprice"),
+          modalWindowDesc = document.querySelector(".modal__window-desc");
+    let activePage, imgBlock, modalImgSrc, modalTitle, modalNewPrice, modalDuration, modalOldPrice, modalDesc;
     menu.addEventListener("click", function () {
         if (!menuItem1.classList.contains("active")) {
             menuItem1.classList.add("active");
@@ -83,4 +92,44 @@ document.addEventListener("DOMContentLoaded", () => {
             productsPages[b].classList.add("active");
         });
     }
+    for (let k = 0; k < productsItem.length; k++) {
+        productsItem[k].addEventListener("click", function () {
+            modalWindow.style.top = (window.pageYOffset - 20) + "px";
+            document.body.style.overflow = "hidden";
+            modalWindow.classList.remove("hide");
+            modalWindow.classList.add("active");
+            imgBlock = productsItem[k].querySelector(".product__item-img");
+            modalImgSrc = imgBlock.children[0].getAttribute("src");
+            modalTitle = productsItem[k].querySelector(".product__item-title").textContent;
+            modalNewPrice = productsItem[k].querySelector(".newprice__price").textContent;
+            if (productsItem[k].children[1].children[0].children[0].children[1]) {
+                modalDuration = productsItem[k].children[1].children[0].children[0].children[1].textContent;
+                modalOldPrice = productsItem[k].children[1].children[0].children[1].textContent;
+            } else {
+                modalDuration = "";
+                modalOldPrice = "";
+            }
+            modalDesc = productsItem[k].querySelector(".product__item-descr").textContent;
+            modalWindowImg.innerHTML = `<img src=\"${modalImgSrc}\" alt=\"modal-img\">`;
+            modalWindowTitle.innerHTML = modalTitle;
+            modalWindowNewprice.innerHTML = modalNewPrice;
+            if (modalDuration) {
+                modalWindowdDuration.innerHTML = modalDuration;
+                modalWindowOldprice.innerHTML = modalOldPrice;
+            } else {
+                modalWindowdDuration.innerHTML = "";
+                modalWindowOldprice.innerHTML = "";
+            }
+            modalWindowDesc.innerHTML = modalDesc;
+            modalOpacity.classList.remove("hide");
+            modalOpacity.classList.add("active");
+        });
+    }
+    modalOpacity.addEventListener("click", function () {
+        document.body.style.overflow = "";
+        modalWindow.classList.remove("active");
+        modalWindow.classList.add("hide");
+        modalOpacity.classList.remove("active");
+        modalOpacity.classList.add("hide");
+    });
 });
